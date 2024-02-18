64c88f76737e647587af9fe88021fa62848c6637
Katia <valentk17@gmail.com>
2024-02-17 21:58:42 -0300

    Nuevo cambio, sin avances, mas dudas

--- app.js
+++ app.js
@@ -1,27 +1,68 @@
 
-// const keys = document.querySelectorAll(".key");
-const numberBtns = document.querySelectorAll("[data-number]");
-const operatorBtns = document.querySelectorAll("[data-operator]");
-const clearBtn = document.querySelectorAll("[data-clear]");
-const equalBtn = document.querySelector("[data-equal]");
+const keys = document.querySelectorAll("[data-key]");
+// const numberBtns = document.querySelectorAll("[data-number]");
+// const operatorBtns = document.querySelectorAll("[data-operator]");
+// const clearBtn = document.querySelectorAll("[data-clear]");
+// const equalBtn = document.querySelector("[data-equal]");
 
 const outputPreview = document.getElementById("outputPreview");   //arriba
 const outputResult = document.getElementById("outputResult");     //abajo
 
 // FUNCIONES
-const clearAllOp = () =>{
-   clearBtn.addEventListener("click",()=>{
-      outputPreview.innerText = "";
-      outputResult.innerText = "";
+
+// for (let key of keys){
+//    const valueNum = key.dataset.number;
+//    console.log(valueNum);
+// }
+
+for (let key of keys){
+   // let outputPrev = "";
+   // let outputRes = "";
+   const value = key.dataset.key;
+   const operators = ["-", "+", "/", "%", "="];
+
+   key.addEventListener("click", ()=> {
+      if(value == "AC"){
+         outputPreview.innerText = "";
+         outputResult.innerText = "";
+      }
+      else if(value == "blackspace"){
+         outputPreview.innerText = outputPreview.slice(0, -1);
+         console.log(outputPreview)
+      }
+      else if(value == "="){
+         const resultado =()=>{
+            eval(value)
+         }
+         outputResult.innerText = resultado;
+      }
+      else {
+         outputPreview.innerText += value;
+      }
    });
+
+   // console.log(expReg.exec(value))
+   // console.log(Array.of(value))
 }
 
-numberBtns.forEach((num) =>{
-   num.addEventListener("click", ()=>{
-      outputPreview.innerText += num.innerText;
-      console.log(num.innerText);
-   });
-})
+
+
+
+
+
+// const clearAllOp = () =>{
+//    clearBtn.addEventListener("click",()=>{
+//       outputPreview.innerText = "";
+//       outputResult.innerText = "";
+//    });
+// }
+
+// numberBtns.forEach((num) =>{
+//    num.addEventListener("click", ()=>{
+//       outputPreview.innerText += num.innerText;
+//       console.log(num.innerText);
+//    });
+// })
 
 
 
@@ -30,7 +71,7 @@ numberBtns.forEach((num) =>{
 // const keys = document.querySelector('.clear');
 // let res = keys.getAttribute("data-key");
 // const res = keys.dataset.key;
-// outputPreview.innerHTML += value;
+// outputPreview.innerText += value;
 // .key.operator
 // elemento.className += " col-md-6";
 // "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "0" || "00" || "."
@@ -41,12 +82,12 @@ numberBtns.forEach((num) =>{
 
 //    key.addEventListener("click", ()=> {
 //       if(value == "AC"){
-//          outputPreview.innerHTML = "";
+//          outputPreview.innerText = "";
 //       } else {
-//          outputPreview.innerHTML += value;
+//          outputPreview.innerText += value;
 //       }   
 //    });
 
 //    console.log(expReg.exec(value))
-//    // console.log(Array.of(value))
+//    console.log(Array.of(value))
 // }
